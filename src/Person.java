26e1702a85457a60cd5ee70777f672bb81b302df
Руслан <Stariy123@mail.ru>
2023-07-06 23:13:52 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+import java.security.PublicKey;
+
+public class Person {
+    public String person;
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public Person (String p){
+        person = p;
+    }
+}
